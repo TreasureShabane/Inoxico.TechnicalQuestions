5e9d0f1dca0240cd0892319c044c8cd77d78113a
Treasure Shabane <Treasure.Shabane@senwes.co.za>
2023-03-11 07:35:48 +0200

    Progress

--- Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
@@ -10,6 +10,8 @@ namespace Inoxico.TechnicalQuestions.Answers
                 var n = points.Length;
                 var depth = -1;
                 var i = 0;
+                var j = 0;
+                var k = 0;
                 var p = 0;
                 var q = 0;
                 var r = 0;
@@ -20,6 +22,18 @@ namespace Inoxico.TechnicalQuestions.Answers
 
                 while (i < n - 2)
                 {
+                    p = points[i];
+                    j = i + 1;
+
+                    while (points[j] < points[j - 1] && j < n - 1)
+                    {
+                        j++;
+                    }
+
+                    if (j == i + 1)
+                    {
+                        i++;
+                    }
 
                 }
 			}
