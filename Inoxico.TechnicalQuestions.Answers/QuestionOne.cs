7f7c87bff9f9717dd761b98c1977effd65ade867
Treasure Shabane <Treasure.Shabane@senwes.co.za>
2023-03-11 06:53:14 +0200

    Question one

--- Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
@@ -5,7 +5,24 @@ namespace Inoxico.TechnicalQuestions.Answers
     {
         public static int GetLongestSentance(string s)
         {
-            throw new NotImplementedException();
+            try
+            {
+                int highest = 0;
+                List<string> words = s.Split(".").ToList<string>();
+                foreach (string item in words)
+                {
+                    if(item.Split(" ").Length > highest)
+                    {
+                        highest = item.Split(" ").Length;
+                    }
+                }
+
+                return highest;
+            }
+            catch (Exception)
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
